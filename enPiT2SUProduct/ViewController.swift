31cec8262bfcded2deddbbc86377569653f150dc
Jun Ohkubo <johkubo@mail.saitama-u.ac.jp>
2017-09-15 12:26:03 +0900

    Initial Commit

---
+++ enPiT2SUProduct/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  enPiT2SUProduct
+//
+//  Created by Jun Ohkubo on 2017/09/15.
+//  Copyright © 2017年 enPiT2SU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
