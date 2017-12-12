0afcdf05f2a8d8eedac74fcfc621ef379382d10a
Yusuke-Ikezaki <iappley08@gmail.com>
2017-12-12 16:30:41 +0900

    Update .gitignore

--- enPiT2SUProduct/AppDelegate.swift
+++ enPiT2SUProduct/AppDelegate.swift
@@ -14,14 +14,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     var themeColor = "Orange"
-	let userDefault = UserDefaults.standard
+    let userDefault = UserDefaults.standard
     let color = [
-		"Red": MDCPalette.red.tint700,
-		"Orange": MDCPalette.orange.tint500,
+        "Red": MDCPalette.red.tint700,
+        "Orange": MDCPalette.orange.tint500,
         "Yellow": MDCPalette.yellow.tint500,
-		"Green": MDCPalette.green.tint500,
+        "Green": MDCPalette.green.tint500,
         "Blue": MDCPalette.blue.tint500
-	]
+    ]
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -41,95 +41,88 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         let dict = ["firstLaunch": true]
         self.userDefault.register(defaults: dict)
-		
-		// 初回起動時のみに実行する処理
+        
+        // 初回起動時のみに実行する処理
         if userDefault.bool(forKey: "firstLaunch") {
             userDefault.set(false, forKey: "firstLaunch")
             print("Captioneeが初めて起動されました")
-			// ウォークスルーを表示
-			self.playWalkthrough()
-		}
-		
+            // ウォークスルーを表示
+            self.playWalkthrough()
+        }
+        
         /* 起動時に毎回呼び出される処理 */
-		
+        
         return true
     }
-	
-	/* アプリが初めて起動されたかを判定する */
-	func isFristLaunch() -> Bool {
-		return true
-	}
-	
-	/* ウォークスルーの表示 */
-	func playWalkthrough() {
-		// 背景画像
-		let bgImage = UIImage(named: "AppIcon")
+    
+    /* アプリが初めて起動されたかを判定する */
+    func isFristLaunch() -> Bool {
+        return true
+    }
+    
+    /* ウォークスルーの表示 */
+    func playWalkthrough() {
+        // 背景画像
+        let bgImage = UIImage(named: "AppIcon")
 
-		// 1ページ目の設定
-		let firstPage = OnboardingContentViewController.content(withTitle: "Title 1",
-																body: "Body 1",
-																image: UIImage(named: "Setting"),	// 画像を表示しない場合はnilにする
-																buttonText: "Next",
-																action: nil)
-		firstPage.movesToNextViewController = true
-		
-		// 2ページ目の設定
-		let secondPage = OnboardingContentViewController.content(withTitle: "Title 2",
-																 body: "Body 2",
-																 image: UIImage(named: "Setting"),
-																 buttonText: "Next",
-																 action:nil)
-		secondPage.movesToNextViewController = true
-		
-		// 3ページ目の設定
-		let content3 = OnboardingContentViewController(
-			title: "Title3",
-			body: "Body3",
-			image: nil,
-			buttonText: "さあ、始めよう！",
-			action: {
-				self.getStarted()
-            }
-		)
-		
-		// onboardingViewcontrollerのインスタンスを生成する
-		let walkthroughVC = OnboardingViewController(backgroundImage: bgImage,
-                                                     contents: [content1, content2, content3])
-		// スキップボタンの表示
-		walkthroughVC?.allowSkipping = true
-		// スキップボタンを押した時の動作
-		walkthroughVC?.skipHandler = { self.skip() }
-		
-//		onboardingVC?.shouldMaskBackground = false
-//		onboardingVC?.shouldBlurBackground = true
-//		onboardingVC?.shouldFadeTransitions = true
-//		onboardingVC?.fadePageControlOnLastPage = true
-//		onboardingVC?.fadeSkipButtonOnLastPage = true
-		onboardingVC?.allowSkipping = false
-//		onboardingVC?.skipHandler = { self.skip() }
+        // 1ページ目の設定
+        let firstPage = OnboardingContentViewController.content(withTitle: "Title 1",
+                                                                body: "Body 1",
+                                                                image: UIImage(named: "Setting"),    // 画像を表示しない場合はnilにする
+                                                                buttonText: "Next",
+                                                                action: nil)
+        firstPage.movesToNextViewController = true
+        
+        // 2ページ目の設定
+        let secondPage = OnboardingContentViewController.content(withTitle: "Title 2",
+                                                                 body: "Body 2",
+                                                                 image: UIImage(named: "Setting"),
+                                                                 buttonText: "Next",
+                                                                 action:nil)
+        secondPage.movesToNextViewController = true
+        
+        // 3ページ目の設定
 
-		
-		// onboardingVCを表示
-		window?.rootViewController = onboardingVC
-		print("ウォークスルー表示")
-	}
-	
-	/* ウォークスルーを終了させる */
-	func getStarted() {
-		//Storyboardを指定
-		let storyboard = UIStoryboard(name: "Main", bundle: nil)
-		//MainViewcontrollerを指定
-		let initialViewController = storyboard.instantiateInitialViewController()
-		//rootViewControllerに入れる
-		self.window?.rootViewController = initialViewController
-		//MainVCを表示
-		self.window?.makeKeyAndVisible()
-	}
-	
-	/* ウォークスルー中にskipボタンを押した時の処理 */
-	func skip() {
-		self.getStarted()
-	}
+        let thirdPage = OnboardingContentViewController.content(withTitle: "Title",
+                                                                body: "Body 3",
+                                                                image: UIImage(named: "Setting"),
+                                                                buttonText: "Get Started!",
+                                                                action: { self.getStarted() })
+        
+        // onboardingViewcontrollerのインスタンスを生成
+        let onboardingVC = OnboardingViewController(backgroundImage: bgImage,
+                                                     contents: [firstPage, secondPage, thirdPage])
+        
+//        onboardingVC?.shouldMaskBackground = false
+//        onboardingVC?.shouldBlurBackground = true
+//        onboardingVC?.shouldFadeTransitions = true
+//        onboardingVC?.fadePageControlOnLastPage = true
+//        onboardingVC?.fadeSkipButtonOnLastPage = true
+        onboardingVC?.allowSkipping = false
+//        onboardingVC?.skipHandler = { self.skip() }
+
+        
+        // onboardingVCを表示
+        window?.rootViewController = onboardingVC
+        print("ウォークスルー表示")
+    }
+    
+    /* ウォークスルーを終了させる */
+    func getStarted() {
+        //Storyboardを指定
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        //MainViewcontrollerを指定
+        let initialViewController = storyboard.instantiateInitialViewController()
+        //rootViewControllerに入れる
+        self.window?.rootViewController = initialViewController
+        //MainVCを表示
+        self.window?.makeKeyAndVisible()
+    }
+    
+    /* ウォークスルー中にskipボタンを押した時の処理 */
+    func skip() {
+        self.getStarted()
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
