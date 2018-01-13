955c2987bf2eb630bba6d63b6306d7f278a4e79e
Mikiya Abe <s15ti032@gmail.com>
2018-01-13 16:07:36 +0900

    Enable to Dismissing background tap in translation

--- enPiT2SUProduct/SubViewController.swift
+++ enPiT2SUProduct/SubViewController.swift
@@ -343,10 +343,6 @@ class SubViewController: UIViewController, ItemDelegate {
         alert.addAction(chinese)
         alert.addAction(japanese)
         
-        // ダイアログ外のタップを無効化
-        let presentationController = alert.mdc_dialogPresentationController
-        presentationController?.dismissOnBackgroundTap = false;
-        
         // Alertを表示
         present(alert, animated: true, completion: nil)
     }
