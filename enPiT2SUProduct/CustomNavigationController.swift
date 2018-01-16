92c78849918f6a54a143a58c98bce9e4e9621d96
Yusuke-Ikezaki <iappley08@gmail.com>
2018-01-16 20:04:38 +0900

    Create CustomNavigationController.swift

---
+++ enPiT2SUProduct/CustomNavigationController.swift
@@ -0,0 +1,25 @@
+//
+//  CustomNavigationController.swift
+//  enPiT2SUProduct
+//
+//  Created by team-E on 2018/01/16.
+//  Copyright © 2018年 enPiT2SU. All rights reserved.
+//
+
+import UIKit
+import Material
+import MaterialComponents
+
+class CustomNavigationController: NavigationController {
+    open override func prepare() {
+        super.prepare()
+        isMotionEnabled = true
+        
+        guard let navigationBar = navigationBar as? NavigationBar else {
+            return
+        }
+        
+        navigationBar.depthPreset = .none
+        navigationBar.dividerColor = MDCPalette.orange.tint500
+    }
+}
